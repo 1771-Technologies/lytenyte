1088250e01a691142b8e172e2ed3bc9778086229
Lee Beydoun <leebeydoun@1771technologies.com>
2025-01-04 13:23:18 +0000

    added content

--- community/react-popover/e2e/popover-placement.e2e.ts
+++ community/react-popover/e2e/popover-placement.e2e.ts
@@ -1,22 +1,22 @@
 import { test, expect, AxeBuilder } from "@1771technologies/aio/playwright";
 import { getUrl } from "./get-url.js";
 
-test("should open the popover with the correct placements", async ({ page }) => {
-  const placements = [
-    "left",
-    "left-start",
-    "left-end",
-    "right",
-    "right-start",
-    "right-end",
-    "top",
-    "top-start",
-    "top-end",
-    "bottom",
-    "bottom-start",
-    "bottom-end",
-  ];
+const placements = [
+  "left",
+  "left-start",
+  "left-end",
+  "right",
+  "right-start",
+  "right-end",
+  "top",
+  "top-start",
+  "top-end",
+  "bottom",
+  "bottom-start",
+  "bottom-end",
+];
 
+test("should open the popover with the correct placements", async ({ page }) => {
   await page.goto(getUrl(""));
 
   const select = page.locator("select");
@@ -33,6 +33,33 @@ test("should open the popover with the correct placements", async ({ page }) => 
 
     await expect(dialog).toBeVisible();
     await expect(page).toHaveScreenshot(`placement-${placement}.png`);
+
+    expect((await scanner.analyze()).violations).toEqual([]);
+
+    await dialog.press("Escape");
+    await expect(dialog).not.toBeVisible();
+  }
+
+  expect((await scanner.analyze()).violations).toEqual([]);
+});
+
+test("should open the popover with the correct placements with arrow", async ({ page }) => {
+  await page.goto(getUrl("with-arrow"));
+
+  const select = page.locator("select");
+  const open = page.getByRole("button");
+
+  const scanner = new AxeBuilder({ page });
+  for (const placement of placements) {
+    await select.selectOption(placement);
+    await expect(select).toHaveValue(placement);
+
+    await open.click();
+
+    const dialog = page.getByRole("dialog");
+
+    await expect(dialog).toBeVisible();
+    await expect(page).toHaveScreenshot(`with-arrow-placement-${placement}.png`);
 
     expect((await scanner.analyze()).violations).toEqual([]);
 
