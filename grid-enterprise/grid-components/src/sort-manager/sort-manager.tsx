c612aca507f8041db22c18aaf4aca8648566fbb7
Lee Beydoun <leebeydoun@1771technologies.com>
2025-01-09 22:38:24 +0000

    added content

---
+++ grid-enterprise/grid-components/src/sort-manager/sort-manager.tsx
@@ -0,0 +1,10 @@
+import type { StoreEnterpriseReact } from "@1771technologies/grid-types";
+
+export interface SortManagerProps<D> {
+  readonly grid: StoreEnterpriseReact<D>;
+}
+
+export function SortManager<D>(props: SortManagerProps<D>) {
+  void props;
+  return <div></div>;
+}
