a93b549e7ae0ccaac1c8b7ddb1a03883eca05c5a
Lee Beydoun <leebeydoun@1771technologies.com>
2025-01-12 01:20:40 +0000

    added content

--- grid-enterprise/grid-components/src/sort-manager/sort-manager.tsx
+++ grid-enterprise/grid-components/src/sort-manager/sort-manager.tsx
@@ -1,4 +1,31 @@
 import type { StoreEnterpriseReact } from "@1771technologies/grid-types";
+import type { SelectProps } from "../select/select";
+import type { ReactNode } from "react";
+
+export interface SortManagerConfiguration {
+  readonly columnSelectComponent?: (s: SelectProps) => ReactNode;
+  readonly sortSelectComponent?: (s: SelectProps) => ReactNode;
+  readonly sortDirectionComponent?: (s: SelectProps) => ReactNode;
+  readonly sortDeleteComponent?: () => ReactNode;
+
+  readonly localization: {
+    readonly sortTitle: string;
+    readonly columnLabel: string;
+    readonly sortLabel: string;
+    readonly orderLabel: string;
+    readonly sortApplyLabel: string;
+    readonly sortCancelLabel: string;
+  };
+
+  readonly axe: {
+    readonly ar: string;
+  };
+
+  readonly icons: {
+    readonly dropdownChevron?: () => ReactNode;
+    readonly deleteIcon?: () => ReactNode;
+  };
+}
 
 export interface SortManagerProps<D> {
   readonly grid: StoreEnterpriseReact<D>;
