336742aaba7370960c7c5e589dee1acd25204866
Lee Beydoun <leebeydoun@1771technologies.com>
2025-01-12 13:10:09 +0000

    added content

--- grid-enterprise/grid-components/src/buttons/icon-button.tsx
+++ grid-enterprise/grid-components/src/buttons/icon-button.tsx
@@ -2,20 +2,81 @@ import { t } from "@1771technologies/grid-design";
 import { clsx } from "@1771technologies/js-utils";
 import { useTooltip } from "@1771technologies/react-tooltip";
 import { refCompat } from "@1771technologies/react-utils";
-import { useId, type JSX } from "react";
+import {
+  useId,
+  useMemo,
+  type FocusEvent,
+  type JSX,
+  type KeyboardEvent,
+  type PointerEvent,
+} from "react";
 
 function IconButtonImpl({
   kind,
   disabledReason,
+  onFocus: focus,
+  onBlur: blur,
+  onKeyDown: keydown,
+  onPointerEnter: enter,
+  onPointerLeave: leave,
+  disabled,
+
   ...props
 }: JSX.IntrinsicElements["button"] & { kind: "ghost" | "normal"; disabledReason?: string }) {
   const id = useId();
-  const tooltip = useTooltip(id, <div>{disabledReason}</div>);
+  const {
+    onBlur: tBlur,
+    onFocus: tFocus,
+    onKeyDown: tKeydown,
+    onPointerEnter: tEnter,
+    onPointerLeave: tLeave,
+  } = useTooltip(id, <div>{disabledReason}</div>);
+
+  const mergedProps = useMemo(() => {
+    if (!disabled || !disabledReason) return {};
+
+    const onBlur = (ev: FocusEvent<HTMLButtonElement>) => {
+      blur?.(ev);
+      tBlur();
+    };
+    const onFocus = (ev: FocusEvent<HTMLButtonElement>) => {
+      focus?.(ev);
+      tFocus(ev);
+    };
+    const onKeyDown = (ev: KeyboardEvent<HTMLButtonElement>) => {
+      keydown?.(ev);
+      tKeydown(ev);
+    };
+    const onPointerEnter = (ev: PointerEvent<HTMLButtonElement>) => {
+      enter?.(ev);
+      tEnter(ev);
+    };
+    const onPointerLeave = (ev: PointerEvent<HTMLButtonElement>) => {
+      leave?.(ev);
+      tLeave();
+    };
+
+    return { onBlur, onFocus, onKeyDown, onPointerEnter, onPointerLeave };
+  }, [
+    blur,
+    disabled,
+    disabledReason,
+    enter,
+    focus,
+    keydown,
+    leave,
+    tBlur,
+    tEnter,
+    tFocus,
+    tKeydown,
+    tLeave,
+  ]);
 
   return (
     <button
       {...props}
-      {...(props.disabled ? tooltip : {})}
+      {...mergedProps}
+      disabled={disabled}
       className={clsx(
         css`
           display: flex;
