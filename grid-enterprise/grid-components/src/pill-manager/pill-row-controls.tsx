c7232d79e799f3fcb135ba23468b6193f4c2dcca
Lee Beydoun <leebeydoun@1771technologies.com>
2025-01-18 12:10:42 +0000

    added content

--- grid-enterprise/grid-components/src/pill-manager/pill-row-controls.tsx
+++ grid-enterprise/grid-components/src/pill-manager/pill-row-controls.tsx
@@ -1,6 +1,6 @@
 import { clsx } from "@1771technologies/js-utils";
-import { IconButton } from "../buttons/icon-button";
 import { CollapseGroupIcon, ExpandGroupIcon } from "../icons/expand-icon";
+import { t } from "@1771technologies/grid-design";
 
 export function PillRowControls({
   hasOverflow,
@@ -12,21 +12,33 @@ export function PillRowControls({
   expanded: boolean;
 }) {
   return (
-    <div
+    <button
+      disabled={!hasOverflow}
+      onClick={() => onExpand(!expanded)}
       className={clsx(css`
         display: flex;
         align-items: center;
         justify-content: center;
         background-color: var(--lng1771-backgrounds-ui-panel);
+        border: none;
+        border-inline-start: 1px solid ${t.colors.borders_separator};
+        box-sizing: border-box;
+        cursor: pointer;
+
+        &:hover:not(:disabled) {
+          background-color: ${t.colors.borders_button_light};
+        }
+
+        &:disabled {
+          cursor: not-allowed;
+        }
       `)}
     >
-      <IconButton kind="ghost" disabled={!hasOverflow} onClick={() => onExpand(!expanded)}>
-        {expanded ? (
-          <CollapseGroupIcon color={hasOverflow ? "var(--lng1771-text-medium)" : undefined} />
-        ) : (
-          <ExpandGroupIcon color={hasOverflow ? "var(--lng1771-text-medium)" : undefined} />
-        )}
-      </IconButton>
-    </div>
+      {expanded ? (
+        <CollapseGroupIcon color={hasOverflow ? "var(--lng1771-text-medium)" : undefined} />
+      ) : (
+        <ExpandGroupIcon color={hasOverflow ? "var(--lng1771-text-medium)" : undefined} />
+      )}
+    </button>
   );
 }
