8345b407004a7e4ee050c32f316ef2afc80600ba
Lee Beydoun <leebeydoun@1771technologies.com>
2025-01-19 22:07:08 +0000

    added content

--- grid-community/lytenyte-community/src/header/header-cell.tsx
+++ grid-community/lytenyte-community/src/header/header-cell.tsx
@@ -59,19 +59,29 @@ export function HeaderCell({
 
   const gridId = api.getState().gridId.use();
   const dragProps = useDraggable({
-    dragData: () => ({ column, columnIndex }),
+    dragData: () => ({ columns: [column], columnIndex }),
     dragTags: () => [`${gridId}:grid:${column.pin ?? "none"}`],
     placeholder: () => <DragPlaceholder column={column} />,
   });
 
   const { canDrop, isOver, ...dropProps } = useDroppable({
     tags: [`${gridId}:grid:${column.pin ?? "none"}`],
+    onDrop: (p) => {
+      const data = p.getData() as { columns: ColumnCommunityReact<any>[]; columnIndex: number };
+      const dragIndex = data.columnIndex;
+
+      const isBefore = columnIndex < dragIndex;
+      const src = data.columns.map((c) => c.id);
+      const target = column.id;
+
+      if (isBefore) api.columnMoveBefore(src, target);
+      else api.columnMoveAfter(src, target);
+    },
   });
 
   const dragData = dragState.dragData.use();
   const data = dragData?.();
   const dragIndex = ((data as any)?.columnIndex ?? -1) as number;
-
   const isBefore = columnIndex < dragIndex;
 
   const moveProps = api.columnIsMovable(column) ? dragProps : {};
