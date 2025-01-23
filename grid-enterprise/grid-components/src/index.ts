2101f7cd73f32e560267e6e7811f0c6287e43931
Lee Beydoun <leebeydoun@1771technologies.com>
2025-01-23 20:52:24 +0000

    added content

---
+++ grid-enterprise/grid-components/src/index.ts
@@ -0,0 +1,5 @@
+export { GridFrame } from "./grid-frame/grid-frame.js";
+export { FloatingFrameDriver } from "./floating-frame/floating-frame-driver.js";
+export { ColumnMenuDriver } from "./column-menu-driver/column-menu-driver.js";
+export { ContextMenuDriver } from "./context-menu-driver/context-menu-driver.js";
+export { FilterMenuDriver } from "./filter-menu-driver/filter-menu-driver.js";
--- grid-enterprise/lytenyte-grid-enterprise/src/lytenyte-grid-enterprise.tsx
+++ grid-enterprise/lytenyte-grid-enterprise/src/lytenyte-grid-enterprise.tsx
@@ -2,6 +2,13 @@ import type { StoreEnterpriseReact } from "@1771technologies/grid-types";
 import { Viewport } from "@1771technologies/lytenyte-grid-community/internal";
 import { GridProvider } from "./use-grid";
 import { HeaderCellDefault } from "./components/header-cell-default";
+import {
+  GridFrame,
+  FilterMenuDriver,
+  ColumnMenuDriver,
+  ContextMenuDriver,
+  FloatingFrameDriver,
+} from "@1771technologies/grid-components";
 
 export interface LyteNyteGridEnterpriseProps<D> {
   readonly grid: StoreEnterpriseReact<D>;
@@ -10,7 +17,13 @@ export interface LyteNyteGridEnterpriseProps<D> {
 export function LyteNyteGrid<D>({ grid }: LyteNyteGridEnterpriseProps<D>) {
   return (
     <GridProvider value={grid}>
-      <LyteNyteCommunityImpl />
+      <GridFrame grid={grid}>
+        <LyteNyteCommunityImpl />
+        <FilterMenuDriver />
+        <ColumnMenuDriver />
+        <ContextMenuDriver />
+        <FloatingFrameDriver />
+      </GridFrame>
     </GridProvider>
   );
 }
