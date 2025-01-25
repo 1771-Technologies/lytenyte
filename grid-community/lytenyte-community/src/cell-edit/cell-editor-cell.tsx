7f488191f4d696540b3edd75fe213d9f6a99e766
Lee Beydoun <leebeydoun@1771technologies.com>
2025-01-25 18:35:17 +0000

    added content

--- grid-community/lytenyte-community/src/cell-edit/cell-editor-cell.tsx
+++ grid-community/lytenyte-community/src/cell-edit/cell-editor-cell.tsx
@@ -1,12 +1,11 @@
 import type { ColumnCommunityReact } from "@1771technologies/grid-types";
 import type { CellEditLocation, RowNode } from "@1771technologies/grid-types/community";
-import { getFocusableElements, sizeFromCoord } from "@1771technologies/js-utils";
+import { getFocusableElements } from "@1771technologies/js-utils";
 import { useGrid } from "../use-grid";
 import { getCellEditor } from "./cell-get-editor";
-import { useCallback, useMemo, type CSSProperties } from "react";
+import { useCallback } from "react";
 import { cellEditLocation } from "./cell-edit-location";
-import { getTransform } from "../renderer/get-transform";
-import { getRootCell } from "@1771technologies/grid-core";
+import { useCellEditStyle } from "./use-cell-edit-style";
 
 export interface CellEditorCellProps<D> {
   row: RowNode<D>;
@@ -25,81 +24,13 @@ export function CellEditorCell<D>({
   location,
   isActive,
 }: CellEditorCellProps<D>) {
-  const { columnIndex } = location;
   const { state, api } = useGrid();
-  const vpWidth = state.internal.viewportInnerWidth.use();
-  const rtl = state.rtl.use();
 
-  const style = useMemo(() => {
-    const isStart = column.pin === "start";
-    const isEnd = column.pin === "end";
-
-    const x = isEnd
-      ? xPositions[columnIndex] - xPositions.at(-1)! + vpWidth
-      : xPositions[columnIndex];
-
-    const root = getRootCell(api, location.rowIndex, location.columnIndex);
-
-    const rowIndex = root?.rowIndex ?? location.rowIndex;
-    const colIndex = root?.columnIndex ?? location.columnIndex;
-    const rowSpan = root?.rowSpan ?? 1;
-    const colSpan = root?.columnSpan ?? 1;
-
-    const height = sizeFromCoord(rowIndex, yPositions, rowSpan);
-    const width = sizeFromCoord(colIndex, xPositions, colSpan);
-
-    const rowCount = state.internal.rowCount.peek();
-    const rowTopCount = state.internal.rowTopCount.peek();
-    const rowBotCount = state.internal.rowBottomCount.peek();
-
-    const firstBotIndex = rowCount - rowBotCount;
-    const isTop = rowIndex < rowTopCount;
-    const isBot = rowIndex >= rowCount - rowBotCount;
-
-    let paginateOffset = 0;
-    if (state.paginate.peek()) {
-      const [rowStart] = api.paginateRowStartAndEndForPage(state.paginateCurrentPage.peek());
-      paginateOffset = yPositions[rowStart];
-    }
-
-    const y = isBot
-      ? yPositions[rowIndex] - yPositions[firstBotIndex]
-      : isTop
-        ? yPositions[rowIndex]
-        : yPositions[rowIndex] - yPositions[rowTopCount] - paginateOffset;
-
-    const transform = getTransform(x * (rtl ? -1 : 1), y);
-    const style = { width, height, transform, zIndex: 3 } as CSSProperties;
-
-    if (isStart || isEnd) {
-      style.insetInlineStart = "0px";
-      style.position = "sticky";
-      style.zIndex = 5;
-    }
-
-    return style;
-  }, [
-    api,
-    column.pin,
-    columnIndex,
-    location.columnIndex,
-    location.rowIndex,
-    rtl,
-    state.internal.rowBottomCount,
-    state.internal.rowCount,
-    state.internal.rowTopCount,
-    state.paginate,
-    state.paginateCurrentPage,
-    vpWidth,
-    xPositions,
-    yPositions,
-  ]);
+  const style = useCellEditStyle(column, xPositions, yPositions, location);
 
   const Renderer = getCellEditor(api, column, state.columnBase.use());
   const values = state.internal.cellEditActiveEditValues.use();
   const value = values.get(cellEditLocation(location));
-
-  const isValid = api.cellEditIsValueValid(location);
 
   const autoFocus = useCallback(
     (el: HTMLDivElement | null) => {
@@ -137,11 +68,14 @@ export function CellEditorCell<D>({
         state.internal.cellEditActiveLocation.set(location);
       }}
       onBlur={() => {
-        api.cellEditEnd(location);
+        if (state.internal.cellEditActiveEdits.peek().has(cellEditLocation(location)))
+          api.cellEditEnd(location);
+
         state.internal.cellEditActiveLocation.set(null);
       }}
       onKeyDown={(ev) => {
         if (ev.key === "Enter") {
+          // Should go down one row.
           api.cellEditEnd(location);
           ev.preventDefault();
           ev.stopPropagation();
@@ -171,7 +105,7 @@ export function CellEditorCell<D>({
         row={row}
         value={value}
         setValue={setValue}
-        isValid={isValid}
+        isValid={api.cellEditIsValueValid(location)}
       />
     </div>
   );
