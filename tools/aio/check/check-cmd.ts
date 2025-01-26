65383c9a531c16c6a8db91073245dbdc57b6330a
Lee Beydoun <leebeydoun@1771technologies.com>
2025-01-26 15:46:23 +0000

    added content

--- tools/aio/check/check-cmd.ts
+++ tools/aio/check/check-cmd.ts
@@ -1,30 +1,29 @@
 import { Command } from "commander";
-import { getClosestNpmPackage } from "../utils/get-closed-npm-package";
+// import { getClosestNpmPackage } from "../utils/get-closed-npm-package";
 
 export const checkCmd = new Command("check")
   .description("Checks a package for any issues")
   .action(async () => {
-    console.log("\nRunning Type Check\n");
+    console.log("\nRunning Type Check");
     const typeCheckProc = Bun.spawn(["tsc"], { stdio: ["inherit", "inherit", "inherit"] });
     await typeCheckProc.exited;
 
-    console.log("Types look good!");
-
     if (typeCheckProc.exitCode !== 0) process.exit(1);
+    console.log("Types look good!\n");
 
-    console.log("\nRunning Unit Tests\n");
-    const testCheck = Bun.spawn(["aio", "test", "run", "--pass-with-no-tests", "--coverage"], {
-      stdio: ["inherit", "inherit", "inherit"],
-    });
-    await testCheck.exited;
+    // console.log("\nRunning Unit Tests\n");
+    // const testCheck = Bun.spawn(["aio", "test", "run", "--pass-with-no-tests", "--coverage"], {
+    //   stdio: ["inherit", "inherit", "inherit"],
+    // });
+    // await testCheck.exited;
 
-    if (testCheck.exitCode !== 0) process.exit(1);
+    // if (testCheck.exitCode !== 0) process.exit(1);
 
-    const nearestPkg = getClosestNpmPackage(process.cwd())?.replace("package.json", "");
-    if (!nearestPkg) {
-      console.error("Failed to determine the npm package for this project");
-      process.exit(1);
-    }
+    // const nearestPkg = getClosestNpmPackage(process.cwd())?.replace("package.json", "");
+    // if (!nearestPkg) {
+    //   console.error("Failed to determine the npm package for this project");
+    //   process.exit(1);
+    // }
 
     // const exists = await Bun.file(`${nearestPkg}/index.html`).exists();
 
