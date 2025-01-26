46b6185a2781380ec4cbcc847410683733f15305
Lee Beydoun <leebeydoun@1771technologies.com>
2025-01-26 11:50:09 +0000

    added content

---
+++ grid-community/lytenyte-community/src/use-lyte-nyte-community.ts
@@ -0,0 +1,39 @@
+import { makeStore } from "@1771technologies/grid-store-community";
+import type { PropsCommunityReact, StoreCommunityReact } from "@1771technologies/grid-types";
+import { useEffect, useState, type ReactNode } from "react";
+
+type ChangeReturnType<F extends (...args: any[]) => any, R> = (...args: Parameters<F>) => R;
+
+export const useLyteNyteCommunity = <D>(p: PropsCommunityReact<D>) => {
+  const [grid] = useState(() => {
+    const s = makeStore<D, ReactNode>(p);
+
+    const useSignalWatcher = (
+      c: keyof Omit<StoreCommunityReact<D>["state"], "internal">,
+      fn: () => void,
+    ) => {
+      const signal = s.state[c];
+
+      useEffect(() => {
+        const unsub = signal.watch(fn);
+
+        return () => unsub();
+      }, [fn, signal]);
+    };
+
+    const useEvent: ChangeReturnType<StoreCommunityReact<D>["api"]["eventAddListener"], void> = (
+      ev,
+      fn,
+    ) => {
+      useEffect(() => {
+        const unsub = s.api.eventAddListener(ev, fn);
+
+        return () => unsub();
+      }, [ev, fn]);
+    };
+
+    return { ...s, useSignalWatcher, useEvent };
+  });
+
+  return grid;
+};
