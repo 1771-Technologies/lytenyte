77adda4c2138560375bb0b8d4efaa9dd56174123
roarlyte1771 <roarlyte@1771technologies.com>
2025-03-19 08:04:57 -0500

    Removed filters

--- grid-enterprise/lytenyte-grid-enterprise/src/overlay/loading-overlay.tsx
+++ grid-enterprise/lytenyte-grid-enterprise/src/overlay/loading-overlay.tsx
@@ -1,27 +1,9 @@
-import { t } from "@1771technologies/grid-design";
+import "./loading-overlay.css";
 
 export function LoadingOverlay() {
   return (
-    <div
-      className={css`
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 100%;
-        width: 100%;
-        overflow: hidden;
-      `}
-    >
-      <div
-        className={css`
-          padding: ${t.spacing.space_30} ${t.spacing.space_50};
-          background-color: ${t.colors.backgrounds_ui_panel};
-          border: 1px solid ${t.colors.borders_context_menu};
-          border-radius: ${t.spacing.box_radius_medium};
-        `}
-      >
-        Loading...
-      </div>
+    <div className="lng1771-loading-overlay">
+      <div className="lng1771-loading-overlay__container">Loading...</div>
     </div>
   );
 }
