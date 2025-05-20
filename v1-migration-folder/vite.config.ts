94442f491bb2ab1375cd39291b307c6144453366
RoarLyte <roarlyte@1771technologies.com>
2025-05-20 09:09:29 +0100

    Created the initial v1 migration folder.

---
+++ v1-migration-folder/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["html", "json-summary", "text"],
+      exclude: [
+        "**/_types*.ts",
+        "**/_constants*.ts",
+        "**/*.spec.ts",
+        "**/*.spec.tsx",
+        "**/*.test.ts",
+        "**/*.test.tsx",
+        "**/types.ts",
+        "**/*.bench.ts",
+        "**/index.ts",
+        "**/__tests__/**",
+        "**/scripts/**",
+        "**/*.stories.*",
+        "**/*.d.ts",
+      ],
+      include: ["**/packages/**/src/**/*.ts", "**/packages/**/src/**/*.tsx"],
+      thresholds: {
+        "100": true,
+      },
+    },
+  },
+});
