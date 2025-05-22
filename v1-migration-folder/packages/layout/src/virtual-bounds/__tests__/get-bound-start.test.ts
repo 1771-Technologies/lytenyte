7f8986d7d16df693bcfcc5da216029bdf6e47e3f
RoarLyte <roarlyte@1771technologies.com>
2025-05-22 23:07:21 +0100

    Added layout library for handling view layouts that are virtualized

---
+++ v1-migration-folder/packages/layout/src/virtual-bounds/__tests__/get-bound-start.test.ts
@@ -0,0 +1,20 @@
+import { expect, test } from "vitest";
+import { getBoundStart } from "../get-bound-start.js";
+
+test("getBoundStart: should return the correct bound values", () => {
+  const positions = new Uint32Array([0, 30, 60, 90, 120]);
+
+  expect(getBoundStart(positions, 20, 0, 0, 4)).toEqual(0);
+  expect(getBoundStart(positions, 40, 0, 0, 4)).toEqual(1);
+  expect(getBoundStart(positions, 40, 1, 0, 4)).toEqual(0);
+
+  const positionsVariables = new Uint32Array([0, 30, 100, 150, 250]);
+  expect(getBoundStart(positionsVariables, 120, 0, 0, 4)).toEqual(2);
+  expect(getBoundStart(positionsVariables, 170, 0, 0, 4)).toEqual(3);
+  expect(getBoundStart(positionsVariables, 260, 0, 0, 4)).toEqual(4);
+  expect(getBoundStart(positionsVariables, 290, 1, 0, 4)).toEqual(3);
+  expect(getBoundStart(positionsVariables, -20, 1, 0, 4)).toEqual(0);
+
+  expect(getBoundStart(positions, 20, 0, 2, 4)).toEqual(2);
+  expect(getBoundStart(positions, 400, 0, 2, 4)).toEqual(4);
+});
