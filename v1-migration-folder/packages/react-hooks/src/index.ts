d9f1b8ebc42c357408b554d6627c278ccff10d8a
RoarLyte <roarlyte@1771technologies.com>
2025-05-23 07:30:59 +0100

    Added the use-transitioned-open hook

--- v1-migration-folder/packages/react-hooks/src/index.ts
+++ v1-migration-folder/packages/react-hooks/src/index.ts
@@ -1,2 +1,6 @@
 export { useForkRef } from "./use-fork-ref.js";
+
+export { useTransitionedOpen } from "./use-transitioned-open.js";
+export type { TransitionOptions } from "./use-transitioned-open.js";
+
 export { isReactVersionAtLeast } from "./react-version.js";
