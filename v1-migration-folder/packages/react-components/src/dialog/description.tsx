bab29101a62159f1cc8052846c7d3184d92c25e3
RoarLyte <roarlyte@1771technologies.com>
2025-05-24 16:35:47 +0100

    Added aria handling to the dialog and the title and description properties

---
+++ v1-migration-folder/packages/react-components/src/dialog/description.tsx
@@ -0,0 +1,34 @@
+import { useIsoEffect, useSlot, type SlotComponent } from "@1771technologies/lytenyte-react-hooks";
+import type { OpenState } from "./use-transition-effect";
+import { forwardRef, useId, type JSX } from "react";
+import { useDialog } from "./root.use-dialog";
+
+export interface DialogDescriptionProps {
+  readonly slot?: SlotComponent<OpenState>;
+}
+
+export const DialogDescription = forwardRef<
+  HTMLHeadingElement,
+  JSX.IntrinsicElements["h2"] & DialogDescriptionProps
+>(function DialogDescription({ slot, ...props }, forwarded) {
+  const { setDescriptionId, state } = useDialog();
+  const internalId = useId();
+  const id = props.id;
+
+  useIsoEffect(() => {
+    setDescriptionId(id ?? internalId);
+
+    return () => {
+      setDescriptionId(undefined);
+    };
+  }, [id, setDescriptionId]);
+
+  const render = useSlot({
+    props: [props, { id: id ?? internalId }],
+    ref: forwarded,
+    slot: slot ?? <p />,
+    state,
+  });
+
+  return render;
+});
