6f1b16aa82a62c2013e02924d6b87484fb2bae9d
RoarLyte <roarlyte@1771technologies.com>
2025-05-24 15:03:22 +0100

    Added dialog trigger

---
+++ v1-migration-folder/packages/react-components/src/dialog/trigger.tsx
@@ -0,0 +1,29 @@
+import { forwardRef, type JSX } from "react";
+import type { OpenState } from "./use-transition-effect";
+import { useSlot, type SlotComponent } from "@1771technologies/lytenyte-react-hooks";
+import { useDialog } from "./root.use-dialog";
+
+export interface DialogTriggerProps {
+  readonly slot?: SlotComponent<OpenState>;
+}
+
+export const DialogTrigger = forwardRef<
+  HTMLButtonElement,
+  JSX.IntrinsicElements["button"] & DialogTriggerProps
+>(function DialogTrigger({ slot, ...props }, forwarded) {
+  const ctx = useDialog();
+  const defaultProps: JSX.IntrinsicElements["button"] = {
+    onClick: () => {
+      ctx.onOpenChange(!ctx.open);
+    },
+  };
+
+  const renderedSlot = useSlot({
+    props: [defaultProps, props],
+    ref: forwarded,
+    slot: slot ?? <button />,
+    state: ctx.state,
+  });
+
+  return renderedSlot;
+});
