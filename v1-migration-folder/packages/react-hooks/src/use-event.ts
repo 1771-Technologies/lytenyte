7b6d2c800326b225e43edc4ae32af02cc9b8ffed
RoarLyte <roarlyte@1771technologies.com>
2025-05-24 19:20:55 +0100

    Added focuse trapping of dialog component. Added dismiss handling

--- v1-migration-folder/packages/react-hooks/src/use-event.ts
+++ v1-migration-folder/packages/react-hooks/src/use-event.ts
@@ -23,6 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 */
+/* v8 ignore next 1 */ // wtf
 import React, { useInsertionEffect } from "react";
 
 /** Represents any function type with any number of arguments and return type */
