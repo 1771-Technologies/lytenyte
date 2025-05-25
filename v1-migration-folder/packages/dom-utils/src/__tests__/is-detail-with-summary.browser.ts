5b8e88f0c87b8080173cc444fcf403ef435a5bc9
RoarLyte <roarlyte@1771technologies.com>
2025-05-25 22:31:48 +0100

    Cleaned up the dom utils tests and wrapped them all in describe

--- v1-migration-folder/packages/dom-utils/src/__tests__/is-detail-with-summary.browser.ts
+++ v1-migration-folder/packages/dom-utils/src/__tests__/is-detail-with-summary.browser.ts
@@ -1,15 +1,17 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { isDetailsWithSummary } from "../is-detail-with-summary";
 
-test("isDetailWithSummary: should return the correct result", () => {
-  const detail = document.createElement("details");
-  expect(isDetailsWithSummary(detail)).toEqual(false);
+describe("isDetailWithSummary", () => {
+  test("should return the correct result", () => {
+    const detail = document.createElement("details");
+    expect(isDetailsWithSummary(detail)).toEqual(false);
 
-  const summary = document.createElement("summary");
-  detail.appendChild(summary);
+    const summary = document.createElement("summary");
+    detail.appendChild(summary);
 
-  expect(isDetailsWithSummary(detail)).toEqual(true);
+    expect(isDetailsWithSummary(detail)).toEqual(true);
 
-  const notDetail = document.createElement("div");
-  expect(isDetailsWithSummary(notDetail)).toEqual(false);
+    const notDetail = document.createElement("div");
+    expect(isDetailsWithSummary(notDetail)).toEqual(false);
+  });
 });
