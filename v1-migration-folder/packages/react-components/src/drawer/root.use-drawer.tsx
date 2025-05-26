7d3ec5598a423bac23ec424d9e0ae17afd303598
RoarLyte <roarlyte@1771technologies.com>
2025-05-26 07:32:44 +0100

    Offset and swipe functionality to the drawer component.

--- v1-migration-folder/packages/react-components/src/drawer/root.use-drawer.tsx
+++ v1-migration-folder/packages/react-components/src/drawer/root.use-drawer.tsx
@@ -4,6 +4,10 @@ import { useDialog, type DialogContext } from "../dialog/root.use-dialog";
 export interface DrawerContext {
   readonly side: "top" | "bottom" | "end" | "start";
   readonly swipeToClose: boolean;
+  readonly offset:
+    | number
+    | [block: number, inline: number]
+    | [top: number, end: number, bottom: number, start: number];
 }
 
 export const context = createContext<DrawerContext>(null as unknown as DrawerContext);
