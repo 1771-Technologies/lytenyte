644dab1763fbf10cf4df8baed96d2d8c0aeb3b30
RoarLyte <roarlyte@1771technologies.com>
2025-05-26 07:32:44 +0100

    Offset and swipe functionality to the drawer component.

--- v1-migration-folder/packages/react-components/src/drawer/root.tsx
+++ v1-migration-folder/packages/react-components/src/drawer/root.tsx
@@ -5,13 +5,20 @@ import { context } from "./root.use-drawer";
 export interface DrawerRootProps extends DialogRootProps {
   readonly side?: "top" | "bottom" | "start" | "end";
   readonly swipeToClose?: boolean;
+  readonly offset?:
+    | number
+    | [block: number, inline: number]
+    | [top: number, end: number, bottom: number, start: number];
 }
 
 export const DrawerRoot = forwardRef<DialogApi, PropsWithChildren<DrawerRootProps>>(
-  function DrawerRoot({ side, swipeToClose, children, ...props }, forwarded) {
+  function DrawerRoot(
+    { side = "start", swipeToClose = true, offset = 0, children, ...props },
+    forwarded,
+  ) {
     const value = useMemo(() => {
-      return { side: side ?? "start", swipeToClose: swipeToClose ?? true };
-    }, [side, swipeToClose]);
+      return { side, swipeToClose, offset };
+    }, [offset, side, swipeToClose]);
 
     return (
       <DialogRoot {...props} ref={forwarded}>
