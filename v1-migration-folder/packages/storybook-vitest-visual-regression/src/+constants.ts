54940c0afb16399486568824005633b97a167415
RoarLyte <roarlyte@1771technologies.com>
2025-05-28 23:18:02 +0100

    Added the ability to run visual regression tests and update snapshots from the UI

--- v1-migration-folder/packages/storybook-vitest-visual-regression/src/+constants.ts
+++ v1-migration-folder/packages/storybook-vitest-visual-regression/src/+constants.ts
@@ -1,5 +1,9 @@
 export const NAME = "storybook-vitest-visual-regression";
 export const SVVR_ID = `${NAME}/panel`;
 
+export const SVVR_RERUN = "svvr_rerun";
+export const SVVR_UPDATE = "svvr_update";
+export const SVVR_RERUN_LOADING = "svvr_rerun_loading";
+export const SVVR_RERUN_DONE = "svvr_rerun_done";
 export const SVVR_INIT_REQ = "svvr_init_request";
 export const SVVR_INIT_RES = "svvr_inti_response";
