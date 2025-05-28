516e1ccac050d1999dde56800bfbe7f208134f73
RoarLyte <roarlyte@1771technologies.com>
2025-05-28 07:12:12 +0100

    Added initial image diff

--- v1-migration-folder/packages/storybook-vitest-visual-regression/src/image-diff.tsx
+++ v1-migration-folder/packages/storybook-vitest-visual-regression/src/image-diff.tsx
@@ -1,14 +1,41 @@
-import * as React from "react";
+import React, { useState } from "react";
+import { ImageDiff } from "./image";
+import { css } from "goober";
 
 export interface ImageDifferProps {
   readonly expected: string;
   readonly actual: string;
 }
 export function ImageDiffer({ expected, actual }: ImageDifferProps) {
+  const [type, setType] = useState("difference");
+  const [value, setValue] = useState(0);
   return (
-    <>
-      <img src={expected} />
-      <img src={actual} />
-    </>
+    <div
+      className={css`
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+      `}
+    >
+      <div
+        className={css`
+          display: flex;
+          width: 200px;
+          gap: 8px;
+        `}
+      >
+        <input
+          type="range"
+          min={0}
+          max={100}
+          value={value}
+          onChange={(e) => setValue(+e.target.value)}
+        />
+        <button onClick={() => setType("difference")}>Difference</button>
+        <button onClick={() => setType("fade")}>Fade</button>
+        <button onClick={() => setType("swipe")}>Swipe</button>
+      </div>
+      <ImageDiff before={expected} after={actual} type={type} value={value / 100} />
+    </div>
   );
 }
