b5b44b226fd073e3ea9e28a9c6346ebfbb469c6c
RoarLyte <roarlyte@1771technologies.com>
2025-05-30 21:03:34 +0100

    Removed visual regression test framework. Its too flawed

--- v1-migration-folder/packages/react-components/src/drawer/__tests__/drawer.stories.tsx
+++ v1-migration-folder/packages/react-components/src/drawer/__tests__/drawer.stories.tsx
@@ -3,6 +3,11 @@ import "./drawer.css";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Drawer } from "..";
 import type { DrawerRootProps } from "../root";
+import { within } from "@testing-library/react";
+import { useState } from "react";
+import { expect } from "@storybook/test";
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const meta: Meta<DrawerRootProps> = {
   title: "components/Drawer",
@@ -23,9 +28,13 @@ const meta: Meta<DrawerRootProps> = {
 export default meta;
 
 function Base(props: DrawerRootProps) {
+  const [open, setOpen] = useState<"top" | "bottom" | "start" | "end">("start");
   return (
-    <Drawer.Root {...props} timeExit={300} timeEnter={200} offset={20}>
-      <Drawer.Trigger>Open</Drawer.Trigger>
+    <Drawer.Root {...props} side={open} timeExit={300} timeEnter={200} offset={20}>
+      <Drawer.Trigger onClick={() => setOpen("start")}>Left</Drawer.Trigger>
+      <Drawer.Trigger onClick={() => setOpen("end")}>Right</Drawer.Trigger>
+      <Drawer.Trigger onClick={() => setOpen("top")}>Top</Drawer.Trigger>
+      <Drawer.Trigger onClick={() => setOpen("bottom")}>Bottom</Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Panel className="drawer">This is my dialog panel</Drawer.Panel>
       </Drawer.Portal>
@@ -37,4 +46,14 @@ type Story = StoryObj<DrawerRootProps>;
 
 export const Default: Story = {
   render: Base,
+
+  play: async ({ step }) => {
+    const screen = within(document.body);
+
+    await step("Opening left", async () => {
+      screen.getByText("Left").click();
+      await wait(300);
+      await expect(screen.getByText("This is my dialog panel")).toBeVisible();
+    });
+  },
 };
