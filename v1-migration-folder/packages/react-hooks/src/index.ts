b96dca0c8a3abae05ff5ccbd8ae912c76aaba699
RoarLyte <roarlyte@1771technologies.com>
2025-05-31 07:56:11 +0100

    Added the use measure hook

--- v1-migration-folder/packages/react-hooks/src/index.ts
+++ v1-migration-folder/packages/react-hooks/src/index.ts
@@ -1,12 +1,21 @@
-export { useForkRef } from "./use-fork-ref.js";
+export { isReactVersionAtLeast } from "./react-version.js";
+
+export type { UseSlotProps, SlotComponent } from "./use-slot/index.js";
+export { useSlot } from "./use-slot/index.js";
 
 export { useTransitionedOpen } from "./use-transitioned-open.js";
 export type { TransitionOptions } from "./use-transitioned-open.js";
 
-export { isReactVersionAtLeast } from "./react-version.js";
+export { useMeasure } from "./use-measure/use-measure.js";
+export type {
+  UseMeasureOptions,
+  RectReadOnly,
+  UseMeasureResult,
+  UseMeasureState,
+} from "./use-measure/+types.js";
 
-export * from "./use-slot/index.js";
-
+export { useForkRef } from "./use-fork-ref.js";
 export { useEvent } from "./use-event.js";
 export { useIsoEffect } from "./use-iso-effect.js";
 export { usePrevious } from "./use-previous.js";
+export { useOnWindowResize } from "./use-on-window-resize.js";
