2cabab38393abd80471eb11da396c6104c9ebaf3
RoarLyte <roarlyte@1771technologies.com>
2025-06-08 11:49:25 +0100

    Fixed tooltip being in a dialog

--- v1-migration-folder/packages/react-components/src/portal/portal.tsx
+++ v1-migration-folder/packages/react-components/src/portal/portal.tsx
@@ -1,11 +1,15 @@
 import type { PropsWithChildren } from "react";
 import { createPortal } from "react-dom";
+import { useDialog } from "../dialog/root.use-dialog";
 
 export interface PortalProps {
   readonly target?: HTMLElement;
 }
 export function Portal({ target, children }: PropsWithChildren<PortalProps>) {
-  const el = target || document.body;
+  // Will be null if we are not inside of a dialog.
+  const inDialog = useDialog();
+
+  const el = target ?? inDialog?.dialog ?? document.body;
 
   return createPortal(children, el);
 }
