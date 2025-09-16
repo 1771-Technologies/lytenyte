ed374eefc010b052cb81b025f1dd3cb38d2d805b
RoarLyte <roarlyte@1771technologies.com>
2025-09-16 08:00:28 +0100

    Added tab functionality to the docs

--- documentation/components/company/pro-tag.tsx
+++ documentation/components/company/pro-tag.tsx
@@ -6,6 +6,7 @@ export function ProTag({ small }: { small?: boolean }) {
   return (
     <Image
       className={cn("my-0 ml-1 inline-flex w-8", small && "w-6")}
+      style={{ margin: "0px" }}
       src={ProSrc}
       alt="Tag indicating the feature is only available for LyteNyte PRO"
     />
--- documentation/mdx-components.tsx
+++ documentation/mdx-components.tsx
@@ -1,5 +1,12 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
+import { createGenerator } from "fumadocs-typescript";
+import { AutoTypeTable } from "fumadocs-typescript/ui";
+import { Step, Steps } from "fumadocs-ui/components/steps";
+
+const generator = createGenerator();
+
 import { ProTag } from "./components/company/pro-tag";
 
 // use this function to get MDX components, you will need it for rendering MDX
@@ -7,6 +14,10 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
+    ...TabsComponents,
+    AutoTypeTable: (props) => <AutoTypeTable {...props} generator={generator} />,
+    Step,
+    Steps,
     ProTag,
   };
 }
