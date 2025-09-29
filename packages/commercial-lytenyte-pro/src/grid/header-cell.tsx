3dcfa09d8f8e95a6d2e565d3049c8059d9fca00d
RoarLyte <roarlyte@1771technologies.com>
2025-09-29 19:10:34 +0100

    Moved grid code into its own dedicated folder

--- packages/commercial-lytenyte-pro/src/grid/header-cell.tsx
+++ packages/commercial-lytenyte-pro/src/grid/header-cell.tsx
@@ -2,6 +2,7 @@ import { type CSSProperties, type JSX, type ReactNode } from "react";
 import type { HeaderCellFloating, HeaderCellLayout } from "../+types";
 import { type SlotComponent } from "@1771technologies/lytenyte-react-hooks";
 import { HeaderCell as HeaderCellCore } from "@1771technologies/lytenyte-core/yinternal";
+
 export interface HeaderCellProps<T> {
   readonly cell: HeaderCellLayout<T> | HeaderCellFloating<T>;
   readonly resizerAs?: SlotComponent;
