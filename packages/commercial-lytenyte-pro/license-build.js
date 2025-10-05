272c3019733d16d471f962e23524659736fc0d90
RoarLyte <roarlyte@1771technologies.com>
2025-10-05 13:00:55 +0100

    Refactored license package into the pro pacakge

--- packages/commercial-lytenyte-pro/license-build.js
+++ packages/commercial-lytenyte-pro/license-build.js
@@ -1,6 +1,6 @@
-import { copyFileSync, readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 
-const file = readFileSync("./dist/license.js", "utf8");
+const file = readFileSync("./dist/license/license.js", "utf8");
 const newContents = file.replace(
   "globalThis.ISSUE_DATE",
   `new Date("${new Date().toISOString().split("T")[0]}")`,
@@ -8,4 +8,3 @@ const newContents = file.replace(
 const finalContents = newContents.replace(' ?? /* @__PURE__ */ new Date("2023-03-01")', "");
 
 writeFileSync("./dist/license.js", finalContents);
-copyFileSync("./dist/license.js", "../commercial-lytenyte-pro/src/license.js");
