2cbc2d6f50a549d4d5043c8a25da4231e9534862
RoarLyte <roarlyte@1771technologies.com>
2025-10-11 12:38:39 +0100

    Unbalanced rows docs

---
+++ documentation/content/docs/(server-data-loading)/demos/unbalanced-rows/server.tsx
@@ -0,0 +1,45 @@
+import type {
+  DataRequest,
+  DataResponse,
+  DataResponseBranchItem,
+  DataResponseLeafItem,
+} from "@1771technologies/lytenyte-pro/types";
+
+import { data } from "./data";
+
+const sleep = () => new Promise((res) => setTimeout(res, 600));
+
+export async function Server(reqs: DataRequest[]) {
+  // Simulate latency and server work.
+  await sleep();
+
+  return reqs.map((c) => {
+    let pathNode = data;
+    for (const p of c.path) {
+      pathNode = pathNode[p!];
+    }
+
+    const rows = Object.entries(pathNode);
+
+    return {
+      asOfTime: Date.now(),
+      data: rows.map<DataResponseLeafItem | DataResponseBranchItem>(([name, file]) => {
+        if ("kind" in file)
+          return { kind: "leaf", id: name, data: { ...file, name, depth: c.path.length } };
+
+        return {
+          kind: "branch",
+          childCount: Object.values(file).length,
+          data: { name },
+          id: name,
+          key: name,
+        };
+      }),
+      start: 0,
+      end: rows.length,
+      kind: "center",
+      path: c.path,
+      size: rows.length,
+    } satisfies DataResponse;
+  });
+}
