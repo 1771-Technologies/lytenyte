34b2b45ae60d66610df47ebab332c2fd553190af
RoarLyte <roarlyte@1771technologies.com>
2025-10-19 11:48:19 +0100

    Update to horizontal navigation

--- packages/lytenyte-shared/src/navigation-x/navigator.ts
+++ packages/lytenyte-shared/src/navigation-x/navigator.ts
@@ -1,13 +1,22 @@
-import { getActiveElement } from "../dom-utils/index.js";
+import { getActiveElement, getLastTabbable } from "../dom-utils/index.js";
 import type { ScrollIntoViewFn } from "../+types.non-gen.js";
-import type { PositionUnion } from "../+types.js";
+import type { PositionGridCell } from "../+types.js";
 import { handleViewportFocused } from "./key-handler/handle-viewport-focused.js";
+import { handleInnerItemFocus } from "./key-handler/handle-inner-item-focus.js";
+import { nearestFocusable } from "./nearest-focusable.js";
+import { getColIndex, getColSpan } from "./attributes.js";
+import type { PositionState, RootCellFn } from "./+types.js";
+import { BACKOFF_RUNS } from "./constants.js";
+import { runWithBackoff } from "../js-utils/index.js";
+import { queryCell } from "./query.js";
 
 export interface NavigatorParams {
   readonly viewport: HTMLElement;
   readonly gridId: string;
   readonly scrollIntoView: ScrollIntoViewFn;
-  readonly position: { get: () => PositionUnion | null; set: (p: PositionUnion | null) => void };
+  readonly getRootCell: RootCellFn;
+
+  readonly position: PositionState;
 
   readonly nextKey: string;
   readonly prevKey: string;
@@ -17,10 +26,14 @@ export interface NavigatorParams {
   readonly endKey: string;
   readonly pageUpKey: string;
   readonly pageDownKey: string;
+
+  readonly columnCount: number;
 }
 export function navigator({
   viewport,
   scrollIntoView,
+  getRootCell,
+
   gridId,
   position: cp,
   nextKey,
@@ -31,33 +44,40 @@ export function navigator({
   endKey,
   pageDownKey,
   pageUpKey,
+
+  columnCount,
 }: NavigatorParams) {
   const keys = new Set([nextKey, prevKey, endKey, homeKey, upKey, downKey, pageDownKey, pageUpKey]);
 
   const handleKey = (ev: KeyboardEvent) => {
     const key = ev.key;
     const shouldHandle = () => true; // Temp placeholder for eventual handle hook
+    const modified = ev.ctrlKey || ev.metaKey;
+
+    const done = () => {
+      ev.preventDefault();
+      ev.stopPropagation();
+    };
 
     // First is determining if the key should be handled.
     // Handling the key will prevent any further navigation.
     if (!keys.has(key) || !shouldHandle()) return;
 
-    const beforeKey = () => {
-      // Placeholder for now
-    };
+    // Placeholder hooks for now
+    const beforeKey = () => {};
+    const afterKey = () => {};
 
-    const afterKey = () => {
-      ev.preventDefault();
-      ev.stopPropagation();
-    };
+    const active = getActiveElement(document)!;
 
     // If the element itself is focused, then only the down key or next key should do something.
     // The down key and next key do the same thing in this case. They focus the first item in the grid.
     // The element should be focusable. The idea is you tab to it and then use arrow keys to navigate
     // through the elements.
-    if (viewport === getActiveElement(document)) {
+    if (viewport === active) {
       if (key !== nextKey && key !== downKey) return;
       handleViewportFocused({ afterKey, beforeKey, gridId, scrollIntoView, viewport });
+
+      done();
       return;
     }
 
@@ -67,7 +87,8 @@ export function navigator({
     // a grid element. This can happen if the user renders something focusable without using the grid components.
     // If that's the case, we should just ignore it.
     const pos = cp.get();
-    if (!pos) return;
+    const posElement = nearestFocusable(gridId, active!);
+    if (!pos || !posElement) return;
 
     // At this point we have our key and a direction. Some keys can only fire for a given position. For example,
     // pageUp and pageDown must be on a grid cell or full width position.
@@ -76,16 +97,75 @@ export function navigator({
     const isCell = pos.kind === "full-width" || pos.kind === "cell";
     if ((key === pageUpKey || key === pageDownKey) && !isCell) return;
 
-    // The next and previous values for a full width position can only move in a few ways.
-    if ((key === nextKey || key === prevKey) && pos.kind === "full-width") {
-      //
-    }
+    if (key === nextKey || key == prevKey) {
+      const isBack = key === prevKey;
+      if (pos.kind === "full-width") {
+        // Full width rows cycle through their focus items.
+        // -- cycleInnerHook
+        handleInnerItemFocus(posElement, active, isBack, true);
+        done();
+        return;
+      }
 
-    // At this point we have ways to handle the navigation depending on the key being pressed.
-    if (key === nextKey || key === prevKey) {
-      //
-    } else if (key === upKey || key === downKey) {
-      //
+      if (pos.kind === "cell") {
+        // Check if we can cycle inner.
+        // -- cycleInnerHook
+        if (!modified) {
+          const result = handleInnerItemFocus(posElement, active, isBack, false);
+          if (result) {
+            done();
+            return;
+          }
+        }
+
+        const elColSpan = Number.parseInt(getColSpan(posElement)!);
+        const elColIndex = Number.parseInt(getColIndex(posElement)!);
+
+        // Nothing to do
+        if ((elColIndex === 0 && isBack) || (elColIndex + elColSpan >= columnCount && !isBack))
+          return;
+
+        const nextIndex = isBack
+          ? modified
+            ? 0
+            : elColIndex - 1
+          : modified
+            ? columnCount - 1
+            : elColIndex + elColSpan;
+
+        const root = getRootCell(pos.rowIndex, nextIndex) as PositionGridCell | null;
+        console.log(pos.rowIndex);
+        if (!root) return;
+
+        const { colIndex, rowIndex } = root.root ?? root;
+        scrollIntoView({ column: nextIndex, behavior: "instant" });
+
+        done();
+
+        // The next cell to focus may be out virtualized out of the view, and hence not mounted to the DOM. We run with
+        // a bit of backoff to give the cell some time to render into view.
+        runWithBackoff(() => {
+          const cell = queryCell(gridId, rowIndex, colIndex, viewport);
+
+          if (!cell) return false;
+
+          // If we are moving backward we focus the last tabbable if present otherwise we focus the cell itself.
+          if (isBack) {
+            // -- cycleInnerHook
+            const last = getLastTabbable(cell);
+            if (last) {
+              last.focus();
+            } else {
+              cell.focus();
+              cp.set((prev) => ({ ...prev, rowIndex: pos.rowIndex }) as PositionGridCell);
+            }
+          } else {
+            cell.focus();
+            cp.set((prev) => ({ ...prev, rowIndex: pos.rowIndex }) as PositionGridCell);
+          }
+          return true;
+        }, BACKOFF_RUNS());
+      }
     }
   };
 
