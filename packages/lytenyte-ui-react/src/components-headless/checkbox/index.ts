ff343a238f5f22aaf8adbd4759c29d10f089d04d
RoarLyte <roarlyte@1771technologies.com>
2025-11-10 08:32:54 +0000

    Checkbox component

---
+++ packages/lytenyte-ui-react/src/components-headless/checkbox/index.ts
@@ -0,0 +1,11 @@
+import { Checkmark } from "./checkmark.js";
+import { Container } from "./container.js";
+import { Input } from "./input.js";
+import { Root } from "./root.js";
+
+export const Checkbox = {
+  Input,
+  Container,
+  Checkmark,
+  Root,
+};
