acaedb42441abd27aecc6193a803b4053facbdc7
RoarLyte <roarlyte@1771technologies.com>
2025-11-16 22:19:15 +0000

    Added page elements to the ui components

---
+++ packages/lytenyte-ui-react/src/page-elements/hero/icon-pivot.tsx
@@ -0,0 +1,51 @@
+export function HeroIconPivot() {
+  return (
+    <svg width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect
+        x="0.5"
+        y="0.5"
+        width="36"
+        height="36"
+        rx="3.5"
+        fill="currentcolor"
+        fillOpacity="0.12"
+      />
+      <rect x="0.5" y="0.5" width="36" height="36" rx="3.5" stroke="currentcolor" />
+      <path
+        d="M11 12.25H25.703"
+        stroke="currentcolor"
+        strokeWidth="1.3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11 18.061H17.5718"
+        stroke="currentcolor"
+        strokeWidth="1.3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11 23.9688H17.5"
+        stroke="currentcolor"
+        strokeWidth="1.3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.5 21.3284L23.3284 21.3284L23.3284 18.4999"
+        stroke="currentcolor"
+        strokeWidth="1.3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M23.3281 24.1567L23.3281 21.3283L26.1566 21.3283"
+        stroke="currentcolor"
+        strokeWidth="1.3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
