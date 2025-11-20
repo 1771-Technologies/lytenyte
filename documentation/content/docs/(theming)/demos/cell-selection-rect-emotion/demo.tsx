833744fb3d9ca2eac8d4ebb5937a1aad5eb297be
RoarLyte <roarlyte@1771technologies.com>
2025-11-20 08:01:59 +0000

    Updated user-select settings

--- documentation/content/docs/(theming)/demos/cell-selection-rect-emotion/demo.tsx
+++ documentation/content/docs/(theming)/demos/cell-selection-rect-emotion/demo.tsx
@@ -42,7 +42,7 @@ export default function CellSelectionRect() {
 
   return (
     <div>
-      <div style={{ height: 500 }}>
+      <div style={{ height: 500 }} className="select-none">
         <Grid.Root grid={grid}>
           <Grid.Viewport>
             <Grid.Header>
