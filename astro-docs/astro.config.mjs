0dfc369e28e285863dada1512975b255902c62d4
RoarLyte <roarlyte@1771technologies.com>
2025-11-28 09:36:05 +0000

    Expressive code for astro docs

--- astro-docs/astro.config.mjs
+++ astro-docs/astro.config.mjs
@@ -4,11 +4,23 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()],
+  integrations: [
+    expressiveCode({
+      themes: ["catppuccin-mocha", "catppuccin-latte"],
+      themeCssSelector: (t) => {
+        if (t.name === "catppuccin-mocha") return `[data-theme="dark"]`;
+
+        return `[data-theme="light"]`;
+      },
+    }),
+    mdx(),
+  ],
 });
