c282f176b1ba0b00cd91a35e86895e6da3c66951
RoarLyte <roarlyte@1771technologies.com>
2025-11-29 15:53:43 +0000

    Column visibility docs

---
+++ documentation/content/docs/(column)/demos/column-visibility_x/demo.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { Grid, useClientRowDataSource } from "@1771technologies/lytenyte-pro";
+import "@1771technologies/lytenyte-pro/grid.css";
+import type { Column } from "@1771technologies/lytenyte-pro/types";
+import { useId } from "react";
+import {
+  ExchangeCell,
+  makePerfHeaderCell,
+  NetworkCell,
+  PercentCell,
+  PercentCellPositiveNegative,
+  SymbolCell,
+  tw,
+} from "./components";
+import type { DEXPerformanceData } from "@1771technologies/grid-sample-data/dex-pairs-performance";
+import { data } from "@1771technologies/grid-sample-data/dex-pairs-performance";
+import { ColumnPills } from "./pill-manager";
+
+const columns: Column<DEXPerformanceData>[] = [
+  { id: "symbol", cellRenderer: SymbolCell, width: 220, name: "Symbol" },
+  { id: "network", cellRenderer: NetworkCell, width: 220, hide: true, name: "Network" },
+  { id: "exchange", cellRenderer: ExchangeCell, width: 220, hide: true, name: "Exchange" },
+
+  {
+    id: "change24h",
+    cellRenderer: PercentCellPositiveNegative,
+    headerRenderer: makePerfHeaderCell("Change", "24h"),
+    name: "Change % 24h",
+  },
+
+  {
+    id: "perf1w",
+    cellRenderer: PercentCellPositiveNegative,
+    headerRenderer: makePerfHeaderCell("Perf %", "1w"),
+    name: "Perf % 1W",
+  },
+  {
+    id: "perf1m",
+    cellRenderer: PercentCellPositiveNegative,
+    headerRenderer: makePerfHeaderCell("Perf %", "1m"),
+    name: "Perf % 1M",
+  },
+  {
+    id: "perf3m",
+    cellRenderer: PercentCellPositiveNegative,
+    headerRenderer: makePerfHeaderCell("Perf %", "3m"),
+    name: "Perf % 3M",
+  },
+  {
+    id: "perf6m",
+    cellRenderer: PercentCellPositiveNegative,
+    headerRenderer: makePerfHeaderCell("Perf %", "6m"),
+    name: "Perf % 6M",
+  },
+  {
+    id: "perfYtd",
+    cellRenderer: PercentCellPositiveNegative,
+    headerRenderer: makePerfHeaderCell("Perf %", "YTD"),
+    name: "Perf % YTD",
+  },
+  { id: "volatility", cellRenderer: PercentCell, name: "Volatility" },
+  {
+    id: "volatility1m",
+    cellRenderer: PercentCell,
+    headerRenderer: makePerfHeaderCell("Volatility", "1m"),
+    name: "Volatility 1M",
+  },
+];
+
+export default function ColumnBase() {
+  const ds = useClientRowDataSource({ data: data });
+
+  const grid = Grid.useLyteNyte({
+    gridId: useId(),
+    rowDataSource: ds,
+    columns,
+    rowHeight: 50,
+    headerGroupHeight: 20,
+    columnBase: { width: 80 },
+  });
+
+  const view = grid.view.useValue();
+
+  return (
+    <div>
+      <ColumnPills grid={grid} />
+      <div className="lng-grid" style={{ height: 500 }}>
+        <Grid.Root grid={grid}>
+          <Grid.Viewport>
+            <Grid.Header>
+              {view.header.layout.map((row, i) => {
+                return (
+                  <Grid.HeaderRow key={i} headerRowIndex={i}>
+                    {row.map((c) => {
+                      if (c.kind === "group") return null;
+
+                      return (
+                        <Grid.HeaderCell
+                          key={c.id}
+                          cell={c}
+                          className={tw(
+                            "text-ln-gray-60! dark:text-ln-gray-70! flex h-full w-full items-center text-nowrap px-2 text-xs capitalize",
+                            c.column.type === "number" && "justify-end",
+                          )}
+                        />
+                      );
+                    })}
+                  </Grid.HeaderRow>
+                );
+              })}
+            </Grid.Header>
+            <Grid.RowsContainer>
+              <Grid.RowsCenter>
+                {view.rows.center.map((row) => {
+                  if (row.kind === "full-width") return null;
+
+                  return (
+                    <Grid.Row row={row} key={row.id}>
+                      {row.cells.map((c) => {
+                        return (
+                          <Grid.Cell
+                            key={c.id}
+                            cell={c}
+                            className="text-[13px]! flex h-full w-full items-center px-2"
+                          />
+                        );
+                      })}
+                    </Grid.Row>
+                  );
+                })}
+              </Grid.RowsCenter>
+            </Grid.RowsContainer>
+          </Grid.Viewport>
+        </Grid.Root>
+      </div>
+    </div>
+  );
+}
