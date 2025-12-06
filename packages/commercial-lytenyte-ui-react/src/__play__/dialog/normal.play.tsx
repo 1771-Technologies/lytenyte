3c2dff056426f2584ba17d5d5e2dc4def655806c
RoarLyte <roarlyte@1771technologies.com>
2025-12-06 15:13:34 +0000

    Styles for the dialog element

--- packages/commercial-lytenyte-ui-react/src/__play__/dialog/normal.play.tsx
+++ packages/commercial-lytenyte-ui-react/src/__play__/dialog/normal.play.tsx
@@ -1,13 +1,21 @@
 import "@1771technologies/lytenyte-design/font.css";
 import "@1771technologies/lytenyte-design/design.css";
+
 import "@1771technologies/lytenyte-design/light.css";
+import "@1771technologies/lytenyte-design/dark.css";
+import "@1771technologies/lytenyte-design/term.css";
+import "@1771technologies/lytenyte-design/teal.css";
+import "@1771technologies/lytenyte-design/cotton-candy.css";
+
+import "@1771technologies/lytenyte-design/shadcn-vars.css";
+import "@1771technologies/lytenyte-design/shadcn.css";
 
 import "../../design/dialog.css";
 import { Dialog } from "../../headless/dialog/index.js";
 
 export default function NormalPlay() {
   return (
-    <div className="ln-dark">
+    <div className="ln-shadcn">
       <Dialog.Root>
         <Dialog.Trigger>Open Dialog</Dialog.Trigger>
         <Dialog.Container>
