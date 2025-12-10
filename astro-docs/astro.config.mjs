44f23f51dfff4c0297d59098dd5102f9dfb15fe6
RoarLyte <roarlyte@1771technologies.com>
2025-12-10 23:10:39 +0000

    Codesandbox results

--- astro-docs/astro.config.mjs
+++ astro-docs/astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import { astroDoc } from "@1771technologies/lytenyte-doc";
+import { lnDoc } from "@1771technologies/lytenyte-doc";
 
 export default defineConfig({
-  integrations: [react(), astroDoc({})],
+  integrations: [
+    react(),
+    lnDoc({
+      githubOrg: "1771-technologies",
+      githubRepo: "lytenyte",
+    }),
+  ],
 });
