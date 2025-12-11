62707244d2c1629c625d7f091d443b8d6af9efdc
RoarLyte <roarlyte@1771technologies.com>
2025-12-11 08:24:44 +0000

    Improved the code frame button controls

---
+++ packages-documentation/lytenyte-doc/src/code-frame/frame-controls.tsx
@@ -0,0 +1,101 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip.js";
+
+export function FrameControls({
+  codeSandbox,
+  files,
+  stackBlitz,
+}: {
+  files: Record<string, string>;
+  codeSandbox: string;
+  stackBlitz: string;
+}) {
+  return (
+    <>
+      <button className="cf-expand-btn center flex cursor-pointer items-center gap-1 rounded-lg px-1.5 py-1 text-[12px] transition-colors hover:bg-gray-200">
+        <span className="iconify ph--arrows-out-simple block size-4 group-data-[expanded=true]:hidden"></span>
+        <span className="iconify ph--arrows-in-simple block size-4 group-data-[expanded=false]:hidden"></span>
+        <span className="hidden group-data-[expanded=false]:block">Expand Code</span>
+        <span className="block group-data-[expanded=false]:hidden">Collapse Code</span>
+      </button>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <button className="center ln-resetter size-7 cursor-pointer rounded-lg transition-colors hover:bg-gray-200">
+              <span className="sr-only">Reset the demo to the initial state</span>
+              <span className="iconify ph--arrows-counter-clockwise-duotone size-4.5"></span>
+            </button>
+          </TooltipTrigger>
+          <TooltipContent>Reset Demo</TooltipContent>
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <a
+              href={codeSandbox}
+              target="_blank"
+              className="center size-7 cursor-pointer rounded-lg transition-colors hover:bg-gray-200"
+            >
+              <span className="sr-only">Fork code on code sandbox</span>
+              <span className="iconify ph--codesandbox-logo-duotone size-4.5"></span>
+            </a>
+          </TooltipTrigger>
+          <TooltipContent>Edit in CodeSandbox</TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <a
+              href={stackBlitz}
+              target="_blank"
+              className="center size-7 cursor-pointer rounded-lg transition-colors hover:bg-gray-200"
+            >
+              <span className="sr-only">Fork code on stack blitz</span>
+              <span className="iconify logos--stackblitz-icon size-4.5"></span>
+            </a>
+          </TooltipTrigger>
+          <TooltipContent>Edit in StackBlitz</TooltipContent>
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <button
+              className="center group relative top-px size-7 cursor-pointer rounded-lg transition-colors hover:bg-gray-200"
+              onClick={(ev) => {
+                const btn = ev.currentTarget;
+
+                let root = btn as HTMLElement;
+                while (root && !root.classList.contains("cf-root")) {
+                  root = root.parentElement!;
+                }
+                if (!root) return;
+
+                const file = Array.from(root.querySelectorAll("li.tab a"))
+                  .filter((x) => x.getAttribute("aria-selected") === "true")
+                  .map((x) => x.textContent.trim())[0];
+
+                if (!file) return;
+
+                const content = files[file];
+                if (!content) return;
+
+                navigator.clipboard.writeText(content);
+
+                btn.setAttribute("data-active", "true");
+                btn.disabled = true;
+
+                setTimeout(() => {
+                  btn.setAttribute("data-active", "false");
+                  btn.disabled = false;
+                }, 2000);
+              }}
+            >
+              <span className="iconify ph--copy-duotone size-4.5 group-data-[active=true]:hidden"></span>
+              <span className="iconify ph--check size-4.5 hidden text-green-500 group-data-[active=true]:block"></span>
+            </button>
+          </TooltipTrigger>
+          <TooltipContent>Copy Code Content</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </>
+  );
+}
---
+++ packages-documentation/lytenyte-doc/src/ui/tooltip.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { Tooltip as TooltipPrimitive } from "radix-ui";
+import * as React from "react";
+import { cn } from "./cn.js";
+
+const TooltipProvider = TooltipPrimitive.Provider;
+const Tooltip = TooltipPrimitive.Root;
+
+const TooltipTrigger = TooltipPrimitive.Trigger;
+
+const TooltipContent = React.forwardRef<
+  React.ComponentRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+  <TooltipPrimitive.Portal>
+    <TooltipPrimitive.Content
+      ref={ref}
+      align={align}
+      sideOffset={sideOffset}
+      side="bottom"
+      className={cn(
+        "origin-(--radix-popover-content-transform-origin) z-50 rounded-lg border border-gray-200 bg-gray-100 p-2 text-sm text-gray-800 shadow-lg backdrop-blur-lg focus-visible:outline-none dark:border-gray-100 dark:bg-gray-200",
+        "transition-opacity data-[state=closed]:opacity-0 data-[state=open]:opacity-100",
+        className,
+      )}
+      {...props}
+    >
+      {props.children}
+      <TooltipPrimitive.Arrow className="fill-gray-200 stroke-gray-100"></TooltipPrimitive.Arrow>
+    </TooltipPrimitive.Content>
+  </TooltipPrimitive.Portal>
+));
+TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+
+export { Tooltip, TooltipProvider, TooltipTrigger, TooltipContent };
