5605129f079842ca756aefe359c91cad05d8f049
RoarLyte <roarlyte@1771technologies.com>
2025-12-11 08:24:44 +0000

    Improved the code frame button controls

--- astro-docs/blog/play/demo.tsx
+++ astro-docs/blog/play/demo.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default function Load() {
   const [count, setCount] = useState(0);
   return (
-    <div className="border bg-red-500 px-2 py-2" onClick={() => setCount((prev) => prev + 1)}>
+    <div className="border px-2 py-2" onClick={() => setCount((prev) => prev + 1)}>
       {count}
     </div>
   );
