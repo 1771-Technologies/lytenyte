4b7fbfae87bdf710e2dfc73bea5ebe9a5ae7f253
RoarLyte <roarlyte@1771technologies.com>
2025-12-23 09:02:35 +0000

    Added remaining api methods for core. This completes Core v2 woop

--- packages/lytenyte-core-experimental/src/constants.ts
+++ packages/lytenyte-core-experimental/src/constants.ts
@@ -1,22 +1,4 @@
-import type { RowSource } from "@1771technologies/lytenyte-shared";
-
 export const EMPTY_POSITION_ARRAY = new Uint32Array();
 export const AnyArray = Object.freeze([]) as any;
 export const AnyObject = Object.freeze({}) as any;
 export const AnySet = Object.freeze(new Set<any>());
-
-export const DEFAULT_ROW_SOURCE: RowSource = {
-  useBottomCount: () => 0,
-  useTopCount: () => 0,
-  useRowCount: () => 0,
-  useSnapshotVersion: () => 1,
-  useMaxRowGroupDepth: () => 0,
-
-  rowIndexToRowId: () => null,
-  rowByIndex: () => ({ get: () => null, useValue: () => null }),
-  rowById: () => null,
-  rowGroupIsExpanded: () => false,
-
-  onRowGroupExpansionsChange: () => {},
-  onRowsUpdated: () => {},
-};
