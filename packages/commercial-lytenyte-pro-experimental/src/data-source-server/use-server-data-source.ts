f50f8022625ec4e3a4f48918f63a7637c3dad128
RoarLyte <roarlyte@1771technologies.com>
2025-12-25 17:15:32 +0000

    Group loading functionality

--- packages/commercial-lytenyte-pro-experimental/src/data-source-server/use-server-data-source.ts
+++ packages/commercial-lytenyte-pro-experimental/src/data-source-server/use-server-data-source.ts
@@ -1,8 +1,7 @@
-import { useEffect, useMemo, useRef } from "react";
+import { useMemo } from "react";
 import type { DataRequest, DataResponse, DataResponsePinned, QueryFnParams } from "./types";
-import { useEvent, usePiece, type Piece } from "@1771technologies/lytenyte-core-experimental/internal";
-import { ServerData, type DataFetcher } from "./server-data.js";
-import { arrayShallow, type RowSource } from "@1771technologies/lytenyte-shared";
+import { usePiece, type Piece } from "@1771technologies/lytenyte-core-experimental/internal";
+import { type RowSource } from "@1771technologies/lytenyte-shared";
 import { useRowByIndex } from "./source/use-row-by-index.js";
 import { useGlobalRefresh } from "../data-source-client/source/use-global-refresh.js";
 import { useRowIndexToRowId } from "./source/use-row-index-to-row-id.js";
@@ -10,6 +9,7 @@ import { useRowIdToRowIndex } from "./source/use-row-id-to-row-index.js";
 import { useRowById } from "./source/use-row-by-id.js";
 import { useOnViewChange } from "./source/use-on-view-change.js";
 import { useSourceState } from "./source/use-source-state.js";
+import { useSource } from "./source/use-source.js";
 
 export interface RowSourceServer<T> extends RowSource<T> {
   isLoading: Piece<boolean>;
@@ -31,79 +31,27 @@ export function useServerDataSource<T, K extends unknown[] = unknown[]>(
 ): RowSourceServer<T> {
   const s = useSourceState(props);
 
-  const prevQueryKeyRef = useRef(null as any);
-  const queryKey = useMemo(() => {
-    const current = prevQueryKeyRef.current;
-    if (!current) {
-      prevQueryKeyRef.current = props.queryKey;
-      return prevQueryKeyRef.current;
-    }
-
-    if (!arrayShallow(current, props.queryKey)) {
-      prevQueryKeyRef.current = props.queryKey;
-    }
-
-    return prevQueryKeyRef.current;
-  }, [props.queryKey]);
-
-  const queryFn = useEvent(props.queryFn);
-  const dataFetcher = useMemo<DataFetcher>(() => {
-    const fetcher: DataFetcher = (req, expansions) => {
-      const model = { rowGroupExpansions: expansions };
-      return queryFn({ model, queryKey, reqTime: Date.now(), requests: req });
-    };
-
-    return fetcher;
-  }, [queryFn, queryKey]);
-
-  const sourceRef = useRef<ServerData>(null as unknown as ServerData);
-  if (!sourceRef.current) {
-    sourceRef.current = new ServerData({
-      defaultExpansion: props.rowGroupDefaultExpansion ?? false,
-      blocksize: props.blockSize ?? 200,
-      expansions: s.expansions,
-      pivotExpansions: {}, // TODO
-      pivotMode: false, // TODO
-      onResetLoadBegin: () => {
-        s.setIsLoading(true);
-        s.setLoadingError(null);
-      },
-      onInvalidate: () => {}, // TODO
-      onResetLoadEnd: () => s.setIsLoading(false),
-      onResetLoadError: (e) => s.setLoadingError(e),
-      onFlatten: (f) => {
-        s.setTopCount(f.top);
-        s.setBotCount(f.bottom);
-        s.setRowCount(f.center + f.top + f.bottom);
-
-        globalSignal(Date.now());
-      },
-    });
-  }
-
-  useEffect(() => {
-    sourceRef.current.dataFetcher = dataFetcher;
-  }, [dataFetcher]);
-
   const isLoading$ = usePiece(s.isLoading);
   const loadError$ = usePiece(s.loadingError);
   const requestsForView$ = usePiece(s.requestsForView, s.setRequestsForView);
 
   const top$ = usePiece(s.topCount);
   const bot$ = usePiece(s.botCount);
+  const maxDepth$ = usePiece(s.maxDepth);
   const rowCount$ = usePiece(s.rowCount);
 
-  // Source from here down
   const globalSignal = useGlobalRefresh();
+  const source = useSource(props, s, globalSignal);
 
-  const { rowByIndex, rowInvalidate } = useRowByIndex<T>(sourceRef.current, globalSignal);
-  const rowById = useRowById<T>(sourceRef.current);
-  const rowIdToRowIndex = useRowIdToRowIndex<T>(sourceRef.current);
-  const rowIndexToRowId = useRowIndexToRowId<T>(sourceRef.current);
-  const onViewChange = useOnViewChange(sourceRef.current, s.requestsForView, s.setRequestsForView);
+  const { rowByIndex, rowInvalidate } = useRowByIndex<T>(source, globalSignal);
+  const rowById = useRowById<T>(source);
+  const rowIdToRowIndex = useRowIdToRowIndex<T>(source);
+  const rowIndexToRowId = useRowIndexToRowId<T>(source);
+  const onViewChange = useOnViewChange(source, s.requestsForView, s.setRequestsForView);
 
+  const setExpansions = s.setExpansions;
   const rowSource = useMemo<RowSourceServer<T>>(() => {
-    const source: RowSourceServer<T> = {
+    const rowSource: RowSourceServer<T> = {
       rowById,
       rowByIndex,
       rowInvalidate,
@@ -112,12 +60,17 @@ export function useServerDataSource<T, K extends unknown[] = unknown[]>(
       rowChildren: () => [],
       rowIsSelected: () => false,
 
-      rowLeafs: () => [],
+      rowLeafs: () => {
+        console.error(
+          "The LyteNyte Grid Server Data Source does not support requesting leaf values for a given row.",
+        );
+        return [];
+      },
       rowParents: (id) => {
         const rowIndex = rowIdToRowIndex(id);
         if (rowIndex == null) return [];
 
-        const ranges = sourceRef.current.flat.rangeTree.findRangesForRowIndex(rowIndex);
+        const ranges = source.flat.rangeTree.findRangesForRowIndex(rowIndex);
         console.log(ranges);
 
         return [];
@@ -128,11 +81,13 @@ export function useServerDataSource<T, K extends unknown[] = unknown[]>(
       useTopCount: () => top$.useValue(),
       useRowCount: () => rowCount$.useValue(),
       useBottomCount: () => bot$.useValue(),
+      useMaxRowGroupDepth: () => maxDepth$.useValue(),
 
-      useMaxRowGroupDepth: () => 0,
       useSnapshotVersion: () => 0,
 
-      onRowGroupExpansionChange: () => {},
+      onRowGroupExpansionChange: (deltaChanges) => {
+        setExpansions((prev) => ({ ...prev, ...deltaChanges }));
+      },
       onRowsSelected: () => {},
       onRowsUpdated: () => {},
       onViewChange,
@@ -141,11 +96,12 @@ export function useServerDataSource<T, K extends unknown[] = unknown[]>(
       requestsForView: requestsForView$,
     };
 
-    return source;
+    return rowSource;
   }, [
     bot$,
     isLoading$,
     loadError$,
+    maxDepth$,
     onViewChange,
     requestsForView$,
     rowById,
@@ -154,6 +110,8 @@ export function useServerDataSource<T, K extends unknown[] = unknown[]>(
     rowIdToRowIndex,
     rowIndexToRowId,
     rowInvalidate,
+    setExpansions,
+    source,
     top$,
   ]);
 
