96cbab958596c635ffc59fa361d28bd152ef1665
Lee <leebeydoun@1771technologies.com>
2025-12-30 13:42:23 +0000

    Headless component parts docs

--- documentation-v2/docs/(grid)/demos/grid-headless-components/demo.tsx
+++ documentation-v2/docs/(grid)/demos/grid-headless-components/demo.tsx
@@ -1,11 +1,13 @@
-"use client";
-
-import "@1771technologies/lytenyte-pro/grid.css";
-import { useClientRowDataSource, Grid } from "@1771technologies/lytenyte-pro";
-import type { Column, RowLayout } from "@1771technologies/lytenyte-pro/types";
+//#start
+import "@1771technologies/lytenyte-pro-experimental/light-dark.css";
+import { Grid, useClientDataSource, ViewportShadows } from "@1771technologies/lytenyte-pro-experimental";
 import { bankDataSmall } from "@1771technologies/grid-sample-data/bank-data-smaller";
-import { useId } from "react";
-import { BalanceCell, DurationCell, NumberCell } from "./components";
+//#end
+
+type BankData = (typeof bankDataSmall)[number];
+export interface GridSpec {
+  readonly data: BankData;
+}
 
 const [under30, under50, over50] = Object.values(
   Object.groupBy(
@@ -18,7 +20,6 @@ const [under30, under50, over50] = Object.values(
   ),
 );
 
-type BankData = (typeof bankDataSmall)[number];
 const finalData = [
   { id: "full-width", label: "Under 30 Years Old" } as unknown as BankData,
   ...under30,
@@ -28,133 +29,90 @@ const finalData = [
   ...over50,
 ];
 
-const columns: Column<BankData>[] = [
-  { id: "job", width: 120 },
-  { id: "age", type: "number", width: 80, cellRenderer: NumberCell },
-  { id: "balance", type: "number", cellRenderer: BalanceCell },
-  { id: "education", groupPath: ["background"] },
-  { id: "marital", groupPath: ["background"] },
-  { id: "default" },
-  { id: "housing" },
-  { id: "loan" },
-  { id: "contact" },
-  { id: "day", type: "number", cellRenderer: NumberCell },
-  { id: "month" },
-  { id: "duration", type: "number", cellRenderer: DurationCell },
-  { id: "poutcome", name: "P Outcome" },
-  { id: "y" },
+const columns: Grid.Column<GridSpec>[] = [
+  { name: "Job", id: "job", width: 120 },
+  { name: "Age", id: "age", type: "number", width: 80, cellRenderer: NumberCell },
+  { name: "Balance", id: "balance", groupPath: ["Background"], type: "number", cellRenderer: BalanceCell },
+  { name: "Education", id: "education", groupPath: ["Background"] },
+  { name: "Marital", id: "marital" },
+  { name: "Default", id: "default" },
+  { name: "Housing", id: "housing" },
+  { name: "Loan", id: "loan" },
+  { name: "Contact", id: "contact" },
+  { name: "Day", id: "day", type: "number", cellRenderer: NumberCell },
+  { name: "Month", id: "month" },
+  { name: "Duration", id: "duration", type: "number", cellRenderer: DurationCell },
 ];
 
-export default function ColumnGroupExpansions() {
-  const ds = useClientRowDataSource({
-    data: finalData,
-    topData: bankDataSmall.slice(0, 2),
-    bottomData: bankDataSmall.slice(2, 4),
-  });
+const base = { width: 100 };
 
-  const grid = Grid.useLyteNyte({
-    gridId: useId(),
-    rowDataSource: ds,
-    columns,
-
-    rowFullWidthPredicate: (r) =>
-      Boolean(r.row.data && "id" in r.row.data && r.row.data.id === "full-width"),
-    rowFullWidthRenderer: (r) => {
-      const data = r.row.data as { label: string };
-
-      return (
-        <div className="text-ln-gray-70 border-ln-gray-20 flex h-full w-full items-center justify-center border-b font-bold">
-          {data.label}
-        </div>
-      );
-    },
-
-    columnBase: {
-      width: 100,
-    },
-  });
-
-  const view = grid.view.useValue();
+const rowFullWidthPredicate: Grid.Props<GridSpec>["rowFullWidthPredicate"] = (r) =>
+  Boolean(r.row.data && "id" in r.row.data && r.row.data.id === "full-width");
+const rowFullWidthRenderer: Grid.Props<GridSpec>["rowFullWidthRenderer"] = (r) => {
+  const data = r.row.data as { label: string };
 
   return (
-    <div className="lng-grid" style={{ height: 500 }}>
-      <Grid.Root grid={grid}>
-        <Grid.Viewport>
-          <Grid.Header>
-            {view.header.layout.map((row, i) => {
-              return (
-                <Grid.HeaderRow key={i} headerRowIndex={i}>
-                  {row.map((c) => {
-                    if (c.kind === "group") {
-                      return (
-                        <Grid.HeaderGroupCell
-                          key={c.idOccurrence}
-                          cell={c}
-                          className="group flex items-center justify-center gap-2 px-2 capitalize"
-                        >
-                          <div>{c.groupPath.at(-1)}</div>
-                        </Grid.HeaderGroupCell>
-                      );
-                    }
+    <div className="text-ln-gray-70 border-ln-gray-20 flex h-full w-full items-center justify-center border-b font-bold">
+      {data.label}
+    </div>
+  );
+};
 
-                    return (
-                      <Grid.HeaderCell
-                        key={c.id}
-                        cell={c}
-                        className="flex h-full w-full items-center px-2 text-sm capitalize"
-                        style={{
-                          justifyContent: c.column.type === "number" ? "flex-end" : "flex-start",
-                        }}
-                      />
-                    );
-                  })}
-                </Grid.HeaderRow>
-              );
-            })}
-          </Grid.Header>
+export default function ColumnGroupExpansions() {
+  const ds = useClientDataSource({
+    data: finalData,
+    topData: bankDataSmall.slice(0, 2),
+    botData: bankDataSmall.slice(2, 4),
+  });
+
+  return (
+    <div className="ln-grid" style={{ height: 500 }}>
+      <Grid
+        rowSource={ds}
+        rowFullWidthPredicate={rowFullWidthPredicate}
+        rowFullWidthRenderer={rowFullWidthRenderer}
+        columns={columns}
+        columnBase={base}
+        slotShadows={ViewportShadows}
+      >
+        <Grid.Viewport>
+          <Grid.Header />
           <Grid.RowsContainer>
-            <RowSection rows={view.rows.top} section="top" />
-            <RowSection rows={view.rows.center} section="center" />
-            <RowSection rows={view.rows.bottom} section="bottom" />
+            <Grid.RowsTop />
+            <Grid.RowsCenter />
+            <Grid.RowsBottom />
           </Grid.RowsContainer>
         </Grid.Viewport>
-      </Grid.Root>
+      </Grid>
     </div>
   );
 }
 
-function RowSection<D = any>({
-  section,
-  rows,
-}: {
-  rows: RowLayout<D>[];
-  section: "top" | "center" | "bottom";
-}) {
-  const Section =
-    section === "top" ? Grid.RowsTop : section === "bottom" ? Grid.RowsBottom : Grid.RowsCenter;
+//#start
+const formatter = new Intl.NumberFormat("en-US", {
+  maximumFractionDigits: 2,
+  minimumFractionDigits: 0,
+});
+export function BalanceCell({ api, row, column }: Grid.T.CellRendererParams<GridSpec>) {
+  const field = api.columnField(column, row);
 
-  return (
-    <Section>
-      {rows.map((row) => {
-        if (row.kind === "full-width")
-          return <Grid.RowFullWidth style={{ borderBottom: 0 }} key={row.id} row={row} />;
-        return (
-          <Grid.Row row={row} key={row.id}>
-            {row.cells.map((c) => {
-              return (
-                <Grid.Cell
-                  key={c.id}
-                  cell={c}
-                  className="flex h-full w-full items-center px-2 text-sm"
-                  style={{
-                    justifyContent: c.column.type === "number" ? "flex-end" : "flex-start",
-                  }}
-                />
-              );
-            })}
-          </Grid.Row>
-        );
-      })}
-    </Section>
-  );
+  if (typeof field === "number") {
+    if (field < 0) return `-$${formatter.format(Math.abs(field))}`;
+
+    return "$" + formatter.format(field);
+  }
+
+  return `${field ?? "-"}`;
 }
+export function DurationCell({ api, row, column }: Grid.T.CellRendererParams<GridSpec>) {
+  const field = api.columnField(column, row);
+
+  return typeof field === "number" ? `${formatter.format(field)} days` : `${field ?? "-"}`;
+}
+
+export function NumberCell({ api, row, column }: Grid.T.CellRendererParams<GridSpec>) {
+  const field = api.columnField(column, row);
+
+  return typeof field === "number" ? formatter.format(field) : `${field ?? "-"}`;
+}
+//#end
