a6b9c3c8796b0b763e3987bbe5b6c17241038495
RoarLyte <roarlyte@1771technologies.com>
2026-01-30 20:45:15 +0000

    Sorting demo

--- documentation-v2/docs/(tree-data)/demos/tree-sorting/demo.tsx
+++ documentation-v2/docs/(tree-data)/demos/tree-sorting/demo.tsx
@@ -2,6 +2,7 @@ import "@1771technologies/lytenyte-pro-experimental/light-dark.css";
 import { Grid, useTreeDataSource } from "@1771technologies/lytenyte-pro-experimental";
 import { data } from "./data.js";
 import { AvatarCell, GroupCell, ModifiedCell, SizeCell } from "./components.jsx";
+import { useState } from "react";
 
 export interface GridSpec {
   readonly data: { kind: string; size: number; modified: string; lastEditedBy: string; permissions: string };
@@ -22,10 +23,26 @@ const columns: Grid.Column<GridSpec>[] = [
 
 const base: Grid.ColumnBase<GridSpec> = { widthFlex: 1, width: 120 };
 
+//!next 5
+const sortBySize: Grid.T.SortFn<GridSpec["data"]> = (left, right) => {
+  const leftData = left.data.size as number | null;
+  const rightData = right.data.size as number | null;
+
+  if (left == null && right == null) return 0;
+  else if (left == null && right != null) return 1;
+  else if (left != null && right == null) return -1;
+  else {
+    return rightData! - leftData!;
+  }
+};
+
 export default function TreeDemo() {
+  const [sort, setSort] = useState<Grid.T.SortFn<any> | null>(null);
+
   const ds = useTreeDataSource({
     data: data,
     rowGroupDefaultExpansion: true,
+    sort,
 
     rowChildrenFn: (x: any) => {
       if (!x.children) return [];
@@ -42,8 +59,30 @@ export default function TreeDemo() {
   });
 
   return (
-    <div className="ln-grid" style={{ height: 500 }}>
-      <Grid rowSource={ds} rowGroupColumn={group} columnBase={base} columns={columns} />
-    </div>
+    <>
+      <div className="border-ln-border flex gap-4 border-b px-4 py-3">
+        <button
+          data-ln-button="website"
+          data-ln-size="md"
+          onClick={() => {
+            setSort(() => sortBySize);
+          }}
+        >
+          Sort: Size
+        </button>
+        <button
+          data-ln-button="website"
+          data-ln-size="md"
+          onClick={() => {
+            setSort(null);
+          }}
+        >
+          Clear Sort
+        </button>
+      </div>
+      <div className="ln-grid" style={{ height: 500 }}>
+        <Grid rowSource={ds} rowGroupColumn={group} columnBase={base} columns={columns} />
+      </div>
+    </>
   );
 }
