47b74a91b18753897de9a3d7f4f2b6501536a447
RoarLyte <roarlyte@1771technologies.com>
2026-02-06 22:02:44 +0000

    Pill manager documentation

--- packages/commercial-lytenyte-pro-experimental/src/components/pill-manager/pill-row.tsx
+++ packages/commercial-lytenyte-pro-experimental/src/components/pill-manager/pill-row.tsx
@@ -1,13 +1,11 @@
 import { forwardRef, useMemo, useState, type JSX, type PropsWithChildren } from "react";
 import { PillRowProvider, type PillRowContext } from "./pill-row.context.js";
 import type { PillRowSpec } from "./types.js";
-import { usePillRoot } from "./root.context.js";
 
 function PillRowBase(
   { row, children, ...props }: PropsWithChildren<PillRow.Props>,
   forwarded: JSX.IntrinsicElements["div"]["ref"],
 ) {
-  const { wrappedRows } = usePillRoot();
   const [expanded, setExpanded] = useState(false);
 
   const value = useMemo<PillRowContext>(() => {
@@ -23,12 +21,9 @@ function PillRowBase(
 
   return (
     <PillRowProvider value={value}>
-      {wrappedRows && (
-        <div {...props} ref={forwarded} data-ln-pill-row data-ln-expanded={expanded}>
-          {children}
-        </div>
-      )}
-      {!wrappedRows && children}
+      <div {...props} ref={forwarded} data-ln-pill-row data-ln-expanded={expanded}>
+        {children}
+      </div>
     </PillRowProvider>
   );
 }
