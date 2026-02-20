9de708fbb18def6117a8af675c37fce2be5f4aa5
RoarLyte <roarlyte@1771technologies.com>
2026-02-20 23:52:03 +0000

    Frame design settings

--- packages-auxiliary/play-frame-2/bin/main.ts
+++ packages-auxiliary/play-frame-2/bin/main.ts
@@ -1,18 +1,10 @@
 import { dev } from "astro";
 import path from "node:path";
 
-const devServer = await dev({
+await dev({
   root: path.resolve(import.meta.dirname, ".."),
   logLevel: "error",
+  devToolbar: { enabled: false },
 });
-
-console.log("I ran");
-
-// await devServer.stop();
-// process.argv.push("dev");
-// process.argv.push(`--silent`);
-// process.argv.push(`--root`);
-// process.argv.push(path.resolve(import.meta.dirname, ".."));
-// process.argv.push(`--config ${path.resolve(import.meta.dirname, "../astro.config.ts")}`);
 
 // await import(astroBin);
