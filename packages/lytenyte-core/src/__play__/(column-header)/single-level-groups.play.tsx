25da4c61dce0d332ed758e543e1dd5b8c45eb031
RoarLyte <roarlyte@1771technologies.com>
2026-03-15 19:26:03 +0000

    Added play files for the column groups

---
+++ packages/lytenyte-core/src/__play__/(column-header)/single-level-groups.play.tsx
@@ -0,0 +1,40 @@
+import "../test.css";
+import { bankDataSmall } from "@1771technologies/grid-sample-data/bank-data-smaller";
+import { Grid, useClientDataSource } from "../../index.js";
+import { ColumnGroupRenderer } from "./column-group-renderer.js";
+
+interface Spec {
+  readonly data: (typeof bankDataSmall)[number];
+}
+
+// Columns are assigned to one of three single-level groups, with a few ungrouped
+// columns interspersed to show mixed layouts.
+const columns: Grid.Column<Spec>[] = [
+  { id: "age", groupPath: ["Demographics"] },
+  { id: "job", groupPath: ["Demographics"] },
+  { id: "marital", groupPath: ["Demographics"] },
+  { id: "education", groupPath: ["Demographics"] },
+  { id: "balance", groupPath: ["Financial"] },
+  { id: "default", groupPath: ["Financial"] },
+  { id: "housing", groupPath: ["Financial"] },
+  { id: "loan", groupPath: ["Financial"] },
+  { id: "contact" },
+  { id: "day", groupPath: ["Campaign"] },
+  { id: "month", groupPath: ["Campaign"] },
+  { id: "duration", groupPath: ["Campaign"] },
+  { id: "campaign", groupPath: ["Campaign"] },
+  { id: "pdays" },
+  { id: "previous" },
+  { id: "poutcome", name: "P Outcome" },
+  { id: "y" },
+];
+
+export default function SingleLevelGroups() {
+  const ds = useClientDataSource({ data: bankDataSmall });
+
+  return (
+    <div style={{ width: "1000px", height: "600px", border: "1px solid black" }} className="with-border">
+      <Grid columns={columns} rowSource={ds} columnGroupRenderer={ColumnGroupRenderer} />
+    </div>
+  );
+}
