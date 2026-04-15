5cc2e09234bfd1a600dedc881c3089df9bf5a21b
Lee Beydoun <leebeydoun@1771technologies.com>
2026-04-15 07:52:58 +0100

    Fix gender icon not appearing in sandboxes

--- documentation-v2/examples/docs/(pivoting)/pivot-basic/src/components.tsx
+++ documentation-v2/examples/docs/(pivoting)/pivot-basic/src/components.tsx
@@ -61,7 +61,9 @@ export function GenderCell({ api, row, column }: Grid.T.CellRendererParams<GridS
     return (
       <div className="flex h-full w-full items-center gap-2">
         <div className="flex size-6 items-center justify-center rounded-full bg-blue-500/50">
-          <span className="iconify ph--gender-male-bold size-4" />
+          <svg width="16" height="16" fill="currentcolor" viewBox="0 0 256 256">
+            <path d="M216,28H168a12,12,0,0,0,0,24h19L154.28,84.74a84,84,0,1,0,17,17L204,69V88a12,12,0,0,0,24,0V40A12,12,0,0,0,216,28ZM146.41,194.46a60,60,0,1,1,0-84.87A60.1,60.1,0,0,1,146.41,194.46Z"></path>
+          </svg>
         </div>
         Male
       </div>
@@ -71,7 +73,9 @@ export function GenderCell({ api, row, column }: Grid.T.CellRendererParams<GridS
     return (
       <div className="flex h-full w-full items-center gap-2">
         <div className="flex size-6 items-center justify-center rounded-full bg-pink-500/50">
-          <span className="iconify ph--gender-female-bold size-4" />
+          <svg width="16" height="16" fill="currentcolor" viewBox="0 0 256 256">
+            <path d="M212,96a84,84,0,1,0-96,83.13V196H88a12,12,0,0,0,0,24h28v20a12,12,0,0,0,24,0V220h28a12,12,0,0,0,0-24H140V179.13A84.12,84.12,0,0,0,212,96ZM68,96a60,60,0,1,1,60,60A60.07,60.07,0,0,1,68,96Z"></path>
+          </svg>
         </div>
         Female
       </div>
