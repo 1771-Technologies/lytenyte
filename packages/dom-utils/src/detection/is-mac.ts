26c15aad3eb1410a1e3aed8b809b71899aecbffe
Lee Beydoun <leebeydoun@1771technologies.com>
2026-05-04 17:43:26 +0100

    Added license comment

--- packages/dom-utils/src/detection/is-mac.ts
+++ packages/dom-utils/src/detection/is-mac.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2025 1771 Technologies
+Copyright 2026 1771 Technologies
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
