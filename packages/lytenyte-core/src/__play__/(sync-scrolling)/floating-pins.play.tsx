d7d6bd868605fb420f6e31dc7cb2151267e645c9
Lee Beydoun <leebeydoun@1771technologies.com>
2026-05-29 08:43:31 +0100

    Pin and floating area settings

---
+++ packages/lytenyte-core/src/__play__/(sync-scrolling)/floating-pins.play.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { useClientDataSource, Grid } from "../../index.js";
+import { bankData } from "@1771technologies/grid-sample-data/bank-data";
+
+interface Spec {
+  readonly data: (typeof bankData)[number];
+}
+
+const columns: Grid.Column<Spec>[] = [
+  { id: "age", pin: "start", width: 120 },
+  { id: "job", pin: "start" },
+  { id: "balance" },
+  { id: "education" },
+  { id: "marital", pin: "end" },
+  { id: "default", pin: "end", width: 110 },
+  { id: "housing" },
+  { id: "loan" },
+  { id: "contact" },
+  { id: "day" },
+  { id: "month" },
+  { id: "duration" },
+  { id: "campaign" },
+  { id: "pdays" },
+  { id: "previous" },
+  { id: "poutcome", name: "P Outcome" },
+  { id: "y" },
+
+  { id: "1loan" },
+  { id: "1contact" },
+  { id: "1day" },
+  { id: "1month" },
+  { id: "1duration" },
+  { id: "1campaign" },
+  { id: "1pdays" },
+  { id: "1previous" },
+  { id: "1poutcome", name: "P Outcome" },
+  { id: "1y" },
+
+  { id: "2loan" },
+  { id: "2contact" },
+  { id: "2day" },
+  { id: "2month" },
+  { id: "2duration" },
+  { id: "2campaign" },
+  { id: "2pdays" },
+  { id: "2previous" },
+  { id: "2poutcome", name: "P Outcome" },
+  { id: "2y" },
+];
+
+const top = bankData.slice(0, 2);
+const bottom = bankData.slice(-2);
+export default function BasicRendering() {
+  const ds = useClientDataSource({
+    data: bankData,
+    topData: top,
+    bottomData: bottom,
+    sort: [{ dim: { id: "education" } }],
+  });
+  const [sync, setSync] = useState(true);
+
+  return (
+    <>
+      <button onClick={() => setSync((prev) => !prev)}>
+        {sync ? "Scrolling In Sync" : "Multi Threaded Scrolling"}
+      </button>
+      <div style={{ width: "100%", height: "95vh", border: "1px solid black" }}>
+        <Grid
+          columns={columns}
+          rowSource={ds}
+          viewportInitialHeight={500}
+          viewportInitialWidth={500}
+          suppressScrollFlash={sync}
+          floatingRowEnabled
+        />
+      </div>
+    </>
+  );
+}
+
+if (import.meta.vitest) {
+  const { test } = import.meta.vitest;
+  // const { userEvent } = await import("vitest/browser");
+  // const { wait, getCellQuery } = await import("../utils.js");
+  // const { render } = await import("vitest-browser-react");
+
+  test("Should be able to switch between sync and multi-threaded scrolling", async () => {});
+  test("Should be able to scroll to the bottom on the grid", async () => {});
+  test("Should be able to scroll midway on the grid", async () => {});
+}
